a22fada7151d098d1d9e610d2e42e6a2c4732442
Guilherme Anterio <anteriotrabalho@gmail.com>
2019-04-13 15:28:54 -0300

    Projeto Concluido IMC

---
+++ Calculo do IMC/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculo_do_IMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double massa, imc, altura;
+            
+
+            
+            
+
+
+
+            Console.WriteLine("Digita a massa:");
+            massa = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digita a altura:");
+            altura = Convert.ToDouble(Console.ReadLine());
+
+            imc = massa / (altura * 2);
+            if (imc >= 18.5) 
+                Console.WriteLine("esta no peso ideal");
+            else
+            if (imc <= 25)
+            
+                Console.WriteLine("esta obeso");
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+            
+
+
+
+            
+        }
+    }
+}
