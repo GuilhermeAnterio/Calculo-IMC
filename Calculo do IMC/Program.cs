3c57e3af424c7219cd77b240264c3bc7c45179d5
Guilherme Anterio <anteriotrabalho@gmail.com>
2019-04-15 17:45:43 -0300

    Projeto Pronto

--- Calculo do IMC/Program.cs
+++ Calculo do IMC/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace Calculo_do_IMC
 {
@@ -13,17 +14,44 @@ namespace Calculo_do_IMC
             double massa, imc, altura;
             
             Console.WriteLine("Digita a massa:");
-            massa = Convert.ToDouble(Console.ReadLine());
+            massa = double.Parse(Console.ReadLine());
             Console.WriteLine("Digita a altura:");
-            altura = Convert.ToDouble(Console.ReadLine());
+            altura = double.Parse(Console.ReadLine());
 
             imc = massa / (altura * 2);
-            if (imc >= 18.5) 
-                Console.WriteLine("Esta no peso ideal");
+            if (imc < 17.0)
+            {
+                Console.WriteLine("Muito Abaixo do Peso: " + imc.ToString("f2", CultureInfo.InvariantCulture) + "IMC");
+            }
+            else if (imc == 17.0 && imc == 18.49)
+            {
+                Console.WriteLine("Abaixo do peso " + imc.ToString("f2", CultureInfo.InvariantCulture) + "IMC");
+            }
+            else if (imc >= 18.5 && imc <= 25.0)
+            {
+                Console.WriteLine("Parabéns esta no peso normal : " + imc.ToString("f2", CultureInfo.InvariantCulture) + "IMC");
+            }
+            else if (imc > 25 && imc <= 30.0)
+            {
+                Console.WriteLine("Acima do peso: " + imc.ToString("f2", CultureInfo.InvariantCulture) + "IMC");
+            }
+            else if (imc > 30.0 && imc <= 35.0)
+            {
+                Console.WriteLine("Você Esta na Obesidade 1: " + imc.ToString("f2", CultureInfo.InvariantCulture) + "IMC");
+            }
+            else if (imc > 35.0 && imc < 40.0)
+            {
+                Console.WriteLine("Você Esta na Obesidade 2 (severa): " + imc);
+            }
             else
-            if (imc <= 25)
-                Console.WriteLine("Esta obeso");
-            Console.WriteLine("Obrigado pela ajuda");
+            {
+                Console.WriteLine("Você Esta na Obesidade 3 (Mórbida): " + imc);
+
+            }
+          
+
+
+               
                
             Console.ReadKey();
 
